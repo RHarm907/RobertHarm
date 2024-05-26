b9cac893dbc27594e9cc29078bbafc0a8f972d6f
RHarm907 <rharm907@gmail.com>
2024-05-25 21:24:31 -0500

    fixed email password

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'Portfolio.RHarm@gmail.com'
-app.config['MAIL_PASSWORD'] = 'DerpDerp1234!'
+app.config['MAIL_PASSWORD'] = 'aeqm gjix uhfz bbuq'
 
 mail = Mail(app)
 
