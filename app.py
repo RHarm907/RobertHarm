22521bd05bc1bf8f3fe42cea8ca4f36d28552823
RHarm907 <rharm907@gmail.com>
2024-05-25 21:07:28 -0500

    Update app.py
    fixed email address on contact me

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+# Configure Flask-Mail for Gmail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'Portfolio.RHarm@gmail.com'
+app.config['MAIL_PASSWORD'] = 'DerpDerp1234!'
+
+mail = Mail(app)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        # Send email
+        msg = Message('New Contact Form Submission', sender='Portfolio.RHarm@gmail.com', recipients=['Portfolio.RHarm@gmail.com'])
+        msg.body = f'Name: {name}\nEmail: {email}\nMessage: {message}'
+        mail.send(msg)
+
+        return redirect(url_for('thank_you'))
+    
+    return render_template('contact.html')
+
+@app.route('/thank-you')
+def thank_you():
+    return 'Thank you for your message!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
